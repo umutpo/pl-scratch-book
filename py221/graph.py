1a295db9c45d9ef00f93a193376c5aef6002089d
TheUnfocused <mutnba@gmail.com>
2023-01-28 13:26:39 -0800

    Changed the implementation to Adjacency List with a new Edge class

--- py221/graph.py
+++ py221/graph.py
@@ -1,126 +1,142 @@
 import unittest
 
+class Edge:
+    """
+    Simple Edge Implementation for Graphs
+    """
+    def __init__(self, beginning, end, weight=1):
+        self.beginning = beginning
+        self.end = end
+        self.weight = weight
+
 class Vertex:
     """
     Simple Vertex Implementation for Graphs
     """
     def __init__(self, value):
         self.value = value
-        self.edges = []
         self.visited = False
-    
-    def add_edge(self, edge):
-        if edge not in self.edges:
-            self.edges.append(edge)
-    
-    def remove_edge(self, edge):
-        if edge in self.edges:
-            self.edges.remove(edge)
 
 class DirectedGraph:
     """
     Directed Graph implementation using Adjacency List with utility functions
     """
     def __init__(self):
-        self.al = []
+        self.al = {}
 
     def get_vertices(self):
-        return self.al
+        return self.al.keys()
+    
+    def get_vertex_edges(self, vertex):
+        return self.al[vertex]
     
     def reset_visited(self):
-        for vertex in self.al:
+        for vertex in self.al.keys():
             vertex.visited = False
     
     def add_vertex(self, vertex):
         if vertex not in self.al:
-            self.al.append(vertex)
-
-    def remove_vertex(self, vertex):
-        if vertex in self.al:
-            for adjacent in vertex.edges:
-                if vertex in adjacent.edges:
-                    adjacent.edges.remove(vertex)
-            
-            self.al.remove(vertex)
+            self.al[vertex] = []
     
-    def add_edge(self, beginning, end):
-        index = self.al.index(beginning)
-        self.al[index].add_edge(end)
-    
-    def remove_edge(self, beginning, end):
-        index = self.al.index(beginning)
-        self.al[index].remove_edge(end)
+    def add_edge(self, edge):
+        if edge.beginning in self.al:
+            self.al[edge.beginning].append(edge)
 
-def breadth_first_search(graph, value):
+def BFS(graph, value):
     """
     Breadth First Search the given graph for a vertex with the given value
     - Time Complexity: O(n + m) where n is the number of vertices and m is the number of edges
     - Space Complexity: O(n) where n is the number of vertices
     """
+    def breadth_first_search(vertex):
+        vertex.visited = True
+
+        queue = [vertex]
+        while len(queue) > 0:
+            current_vertex = queue.pop()
+            
+            if current_vertex.value == value:
+                return current_vertex
+
+            for adjacent_vertex in graph.get_vertex_edges(current_vertex):
+                if not adjacent_vertex.end.visited:
+                    queue.insert(0, adjacent_vertex.end)
+                    adjacent_vertex.end.visited = True
+        
+        return None
+
+    # This for loop is used for searching for a graph with disconnected components
     graph.reset_visited()
-    initial_vertex = graph.get_vertices()[0]
-    initial_vertex.visited = True
-
-    queue = [initial_vertex]
-    while len(queue) > 0:
-        current_vertex = queue.pop()
-        if current_vertex.value == value:
-            return current_vertex
-
-        for adjacent_vertex in current_vertex.edges:
-            if not adjacent_vertex.visited:
-                queue.insert(0, adjacent_vertex)
-                adjacent_vertex.visited = True
-    
+    for vertex in graph.get_vertices():
+        found = breadth_first_search(vertex)
+        if found != None:
+            return found
+        
     return None
 
-def depth_first_search_iterative(graph, value):
+def DFS_iterative(graph, value):
     """
     Iterative Depth First Search the given graph for a vertex with the given value
     - Time Complexity: O(n + m) where n is the number of vertices and m is the number of edges
     - Space Complexity: O(n) where n is the number of vertices
     """
-    graph.reset_visited()
-    initial_vertex = graph.get_vertices()[0]
+    def depth_first_search_iterative(vertex):
+        graph.reset_visited()
 
-    stack = [initial_vertex]
-    while len(stack) > 0:
-        current_vertex = stack.pop()
-        if not current_vertex.visited:
-            current_vertex.visited = True
-            if current_vertex.value == value:
-                return current_vertex
+        stack = [vertex]
+        while len(stack) > 0:
+            current_vertex = stack.pop()
+            if not current_vertex.visited:
+                current_vertex.visited = True
 
-            for adjacent_vertex in current_vertex.edges:
-                stack.append(adjacent_vertex)
+                if current_vertex.value == value:
+                    return current_vertex
+
+                for adjacent_vertex in graph.get_vertex_edges(current_vertex):
+                    stack.append(adjacent_vertex.end)
+        
+        return None
     
+    # This for loop is used for searching for a graph with disconnected components
+    graph.reset_visited()
+    for vertex in graph.get_vertices():
+        found = depth_first_search_iterative(vertex)
+        if found != None:
+            return found
+        
     return None
 
-def depth_first_search_recursive(graph, value):
+def DFS_recursive(graph, value):
     """
     Recursive Depth First Search the given graph for a vertex with the given value
     - Time Complexity: O(n + m) where n is the number of vertices and m is the number of edges
     - Space Complexity: O(n) where n is the number of vertices
     """
-    def _depth_first_search(vertex):
+    def depth_first_search_recursive(vertex):
         vertex.visited = True
 
         if vertex.value == value:
             return vertex
             
-        for adjacent_vertex in vertex.edges:
-            if not adjacent_vertex.visited:
-                found = _depth_first_search(adjacent_vertex)
+        for adjacent_vertex in graph.get_vertex_edges(vertex):
+            if not adjacent_vertex.end.visited:
+                found = depth_first_search_recursive(adjacent_vertex.end)
                 if found != None:
                     return found
 
         return None
 
+    # This for loop is used for searching for a graph with disconnected components
     graph.reset_visited()
-    return _depth_first_search(graph.get_vertices()[0])
+    for vertex in graph.get_vertices():
+        found = depth_first_search_recursive(vertex)
+        if found != None:
+            return found
+        
+    return None
 
 if __name__ == "__main__":
-    def get_test_graph():
+    def get_test_unweighted_graph():
         graph = DirectedGraph()
         a = Vertex('A')
         b = Vertex('B')
@@ -132,21 +148,21 @@ if __name__ == "__main__":
         graph.add_vertex(c)
         graph.add_vertex(d)
         graph.add_vertex(e)
-        graph.add_edge(a, b)
-        graph.add_edge(a, c)
-        graph.add_edge(b, c)
-        graph.add_edge(b, e)
-        graph.add_edge(b, d)
-        graph.add_edge(c, e)
-        graph.add_edge(d, e)
-        graph.add_edge(e, c)
-        graph.add_edge(e, a)
+        graph.add_edge(Edge(a, b))
+        graph.add_edge(Edge(a, c))
+        graph.add_edge(Edge(b, c))
+        graph.add_edge(Edge(b, e))
+        graph.add_edge(Edge(b, d))
+        graph.add_edge(Edge(c, e))
+        graph.add_edge(Edge(d, e))
+        graph.add_edge(Edge(e, c))
+        graph.add_edge(Edge(e, a))
         return graph
 
     class TestGraph(unittest.TestCase):
         def test_all(self):
-            search_algorithms = [breadth_first_search, depth_first_search_iterative, depth_first_search_recursive]
-            graph = get_test_graph()
+            search_algorithms = [BFS, DFS_iterative, DFS_recursive]
+            graph = get_test_unweighted_graph()
             for algorithm in search_algorithms:
                 find_z = algorithm(graph, 'Z')
                 self.assertEqual(find_z, None)
